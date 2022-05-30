bf6e522a8324bfaa2b0857f9aab295b9ed89f5db
CodeRothy <yiwooy@naver.com>
2022-05-30 23:59:05 +0900

    list

--- src/main/java/com/rim/aboardcado/controller/BoardController.java
+++ src/main/java/com/rim/aboardcado/controller/BoardController.java
@@ -6,9 +6,12 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 //@RequestMapping("/board")
 @Controller
@@ -35,5 +38,12 @@ public class BoardController {
     public String write(BoardDto boardDto) {
         boardService.savePost(boardDto);
         return "redirect:/"; // redirect 경로 확인 (requestMapping /board 포함하는지)
+    }
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<BoardDto> boardDtoList = boardService.getBoardList();
+        model.addAttribute("postList", boardDtoList);
+        return "board/list";
     }
 }
--- src/main/java/com/rim/aboardcado/service/BoardService.java
+++ src/main/java/com/rim/aboardcado/service/BoardService.java
@@ -1,12 +1,16 @@
 package com.rim.aboardcado.service;
 
+import com.rim.aboardcado.domain.entity.Board;
 import com.rim.aboardcado.domain.repository.BoardRepository;
 import com.rim.aboardcado.dto.BoardDto;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
@@ -18,5 +22,23 @@ public class BoardService {
     public Long savePost(BoardDto boardDto) {
 
         return boardRepository.save(boardDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public List<BoardDto> getBoardList() {
+        List<Board> boardList = boardRepository.findAll(Sort.by(Sort.Direction.DESC,"id","createdDate"));
+        List<BoardDto> boardDtoList = new ArrayList<>();
+
+        for (Board board : boardList) {
+            BoardDto boardDto = BoardDto.builder()
+                    .id(board.getId())
+                    .author(board.getAuthor())
+                    .title(board.getTitle())
+                    .content(board.getContent())
+                    .createdDate(board.getCreatedDate())
+                    .build();
+            boardDtoList.add(boardDto);
+        }
+        return boardDtoList;
     }
 }
