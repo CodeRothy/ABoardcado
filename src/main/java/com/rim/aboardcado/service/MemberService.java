e56e65eab7ae3cece3e22a930854702071e6fd28
CodeRothy <yiwooy@naver.com>
2022-06-18 19:37:39 +0900

    SS ver.2 (error 500) 토큰추가

--- src/main/java/com/rim/aboardcado/service/MemberService.java
+++ src/main/java/com/rim/aboardcado/service/MemberService.java
@@ -12,7 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
+//@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
